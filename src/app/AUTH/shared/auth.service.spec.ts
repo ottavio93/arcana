5c085f80bfefd482a40ce276758025a2ad21a631
jhon <lopiccoloottavio@gmail.com>
2021-01-22 16:13:57 +0100

    s

--- src/app/AUTH/shared/auth.service.spec.ts
+++ src/app/AUTH/shared/auth.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { AuthService } from './Auth.service';
+import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
   let service: AuthService;
