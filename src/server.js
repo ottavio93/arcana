6fc417f42fa73fb272dd8788232d40c6ac042968
jhon <lopiccoloottavio@gmail.com>
2021-01-23 00:39:36 +0100

    k

--- src/server.js
+++ src/server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/angular-app-heroku'));
+app.use(express.static(__dirname +'dist/arcana'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/arcana'}),
+    res.sendFile(path.join(__dirname+  'dist/arcana')),
 );
 
 // Start the app by listening on the default Heroku port
